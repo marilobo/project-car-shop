60aa41c91147d9ece980ea772b0a640892013f6a
Mariana <mlobosilva@outlook.com>
2023-04-26 11:36:42 -0300

    Feat: Cria Model AbstractODM com método capaz de criar um novo veículo

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,24 @@
+import {
+  Model,
+  models,
+  Schema,
+  model,
+} from 'mongoose';
+
+abstract class AbstractODM<T> {
+  protected model: Model<T>;
+  protected schema: Schema;
+  protected modelName: string;
+
+  constructor(schema: Schema, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, this.schema);
+  }
+
+  public async create(veiculo: T): Promise<T> {
+    return this.model.create({ ...veiculo });
+  }
+}
+
+export default AbstractODM;
